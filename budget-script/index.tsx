9c047dccc2d54baa300c480768dd7e6328bb01dd
Highlander-Maker <highlander1952@protonmail.com>
2022-06-01 12:12:45 +0100

    getBalance

--- budget-script/index.tsx
+++ budget-script/index.tsx
@@ -1,11 +1,18 @@
-import { CosmWasmClient } from "cosmwasm";
+import { CosmWasmClient, QueryClient } from "cosmwasm";
+
 
 // This is your rpc endpoint
 const rpcEndpoint = "https://rpc.juno.highlander-nodes.net:26797/";
 
+
 async function main() {
   const client = await CosmWasmClient.connect(rpcEndpoint);
   console.log(client);
+  await client.getBalance("juno1dru5985k4n5q369rxeqfdsjl8ezutch8y9vuau","ujuno")
+
 }
+
+
+
 
 main();
