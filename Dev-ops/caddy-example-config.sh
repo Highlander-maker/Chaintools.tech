84c718d25bdf2afc9a5284f1495a37d3b603e35a
Highlander-Maker <highlander1952@protonmail.com>
2022-06-01 12:16:32 +0100

    update caddy config

--- Dev-ops/caddy-example-config.sh
+++ Dev-ops/caddy-example-config.sh
@@ -1,3 +1,10 @@
+# remove config and add this as an example config.
+# once you have done so you need to restart service 
+# ```sudo systemctl restart caddy.service```
+# sudo ufw allow 443/tcp comment 'Caddy HTTPS' 
+# this allows caddy to publish services via this port
+
+
 https://rpc.harpoon-3.highlander-nodes.net {
    reverse_proxy http://YOUR-IP-ADD-HERE:26867 {
        header_down Access-Control-Allow-Origin *
